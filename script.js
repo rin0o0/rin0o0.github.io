7c86ef94716be7bc51a7d06b69e40a0b86fc38cf
rin0o0 <129216396+rin0o0@users.noreply.github.com>
2024-07-17 14:10:33 +0800

    added files

---
+++ script.js
@@ -0,0 +1,56 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+document.querySelectorAll(".carousel").forEach((carousel) => {
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel__button"></span>`;
+    });
+  
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+          <div class="carousel__nav">
+              ${buttonsHtml.join("")}
+          </div>
+      `
+    );
+  
+    const buttons = carousel.querySelectorAll(".carousel__button");
+  
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        // un-select all the items
+        items.forEach((item) =>
+          item.classList.remove("carousel__item--selected")
+        );
+        buttons.forEach((button) =>
+          button.classList.remove("carousel__button--selected")
+        );
+  
+        items[i].classList.add("carousel__item--selected");
+        button.classList.add("carousel__button--selected");
+      });
+    });
+  
+    // Select the first item on page load
+    items[0].classList.add("carousel__item--selected");
+    buttons[0].classList.add("carousel__button--selected");
+  });
+  
+
+  var counter = 2
+  setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++
+    if (counter > 5){
+      counter = 1;
+    }
+  }, 4000);
